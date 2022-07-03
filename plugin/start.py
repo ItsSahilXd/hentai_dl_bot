5afe2855e0875fa2489adc13f6472a699ac12eb4
ItsSahilXd <103504075+ItsSahilXd@users.noreply.github.com>
2022-07-03 15:07:39 +0530

    Update start.py

--- plugin/start.py
+++ plugin/start.py
@@ -2,4 +2,4 @@ from pyrogram import *
 from pyrogram.types import *
 
 def start(client, message):
-    message.reply_text("""**/search <hentai name> to search**\n Powered By @MetaVoid""", parse_mode="markdown")
+    message.reply_text("""**/search <hentai name> to search""", parse_mode="markdown")
